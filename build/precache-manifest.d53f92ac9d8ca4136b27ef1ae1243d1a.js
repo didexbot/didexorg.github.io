25f8caa790e6dd03e9e2dd9440085be117328c37
Eliot Krone <krondev@pm.me>
2019-06-20 09:29:52 +0300

    init

---
+++ build/precache-manifest.d53f92ac9d8ca4136b27ef1ae1243d1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23127ce79f16d61099eacb44464d4dc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e264565b84d4283bfea",
+    "url": "/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "ffbe32d4e790e6a5d2f2",
+    "url": "/static/js/2.335f0c66.chunk.js"
+  },
+  {
+    "revision": "2e264565b84d4283bfea",
+    "url": "/static/js/main.c17f74ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
